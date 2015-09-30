9d148e38540e03bc01c6f132355bb0b80ef74850
Jason Leveille <jason@leve.us>
2015-09-30 09:34:50 -0400

    style(cleanup): Adjust code formatting
    Added a missing semi colon in index.js and fixed some spacing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var starWarsNames = require('./starwars-names.json');
 var uniqueRandomArray = require('unique-random-array');
-var getRandomItem = uniqueRandomArray(starWarsNames)
+var getRandomItem = uniqueRandomArray(starWarsNames);
+
 module.exports = {
   all: starWarsNames,
   random: random
